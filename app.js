e53b1af249eaf9cd6306a1807cd0698c162577fb
RogueNPC <jordanbtansay@gmail.com>
2021-10-02 18:48:47 -0700

    [UPDATE] added socket.io connection

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 //App.js
 const express = require('express');
 const app = express();
-//Socket.io has to use the http server
 const server = require('http').Server(app);
 
-//Express View Engine for Handlebars
+//Socket.io
+const io = require('socket.io')(server);
+io.on("connection", (socket) => {
+  console.log("🔌 New user connected! 🔌");
+})
+
 const exphbs  = require('express-handlebars');
 
 // Deviates from the tutorial to fix "views/layouts/" bug
